e75eadc55d061454593e63d42ed5ce10c1609213
PyLukasz <as.lukasz@gmail.com>
2023-01-31 13:54:07 +0100

    Added producers controller

---
+++ Controllers/ProducersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopMVC.Data;
+
+namespace ShopMVC.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProducersController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var producers = _context.Producers!.ToList();
+            return View();
+        }
+    }
+}
