21999700e85c1c36ce36502966dacf7d37f2e755
PyLukasz <as.lukasz@gmail.com>
2023-02-01 13:31:48 +0100

    Created details view, modify all method in actor service to async.

--- Controllers/ActorsController.cs
+++ Controllers/ActorsController.cs
@@ -16,7 +16,7 @@ namespace ShopMVC.Controllers
         public async Task<IActionResult> Index()
         {
             
-            var data = await _service.GetAll();
+            var data = await _service.GetAllAsync();
             return View(data);
         }
 
@@ -33,14 +33,14 @@ namespace ShopMVC.Controllers
             {
                 return View(actor);
             }
-            _service.Add(actor);
+            await _service.AddAsync(actor);
             return RedirectToAction(nameof(Index));
         }
 
         //Get: Actors/Details/1
         public async Task<IActionResult> Details(int id)
         {
-            var actorDetails = _service.GetById(id);
+            var actorDetails = await _service.GetByIdAsync(id);
             if (actorDetails == null)
             {
                 return View("Empty");
